9dcbbd45fadea54dc63a752d570dc40bc5fad2c4
mchavez <miguel_chavez_m@hotmail.com>
2024-06-01 20:22:56 -0600

    ci updates

--- scripts/wait_for_http_200.sh
+++ scripts/wait_for_http_200.sh
@@ -8,7 +8,7 @@
 
 wait-for-url() {
   echo -n "Waiting $1 "
-  timeout --foreground -s TERM 10s bash -c \
+  timeout --foreground -s TERM 180s bash -c \
   'while [[ "$(curl -k -s -o /dev/null -m 3 -L -w ''%{http_code}'' ${0})" != "200" ]];\
    do
    echo -n "."
