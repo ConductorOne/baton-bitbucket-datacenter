c8ca173950832f89c00e7b9cd42d82c65b341b6e
mchavez <miguel_chavez_m@hotmail.com>
2024-06-04 18:39:34 -0600

    Quick changes

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
+wait-for-url() {
+  echo -n "Waiting $1 "
+  timeout --foreground -s TERM 180s bash -c \
+  'while [[ "$(curl -k -s -o /dev/null -m 3 -L -w ''%{http_code}'' ${0})" != "200" ]];\
+   do
+   echo -n "."
+   sleep 1
+   done' ${1}
+   TIMEOUT_RETURN="$?"
+   if [[ "$TIMEOUT_RETURN" == 0 ]]; then
+     echo -e "\n[+] ${1} - 200 OK"
+   else
+     echo -e "\n[-] ${1} - timeout or other error! [$TIMEOUT_RETURN]"
+   fi
+}
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <url>"
-  exit 1
-fi
+clear
+echo -e "URLs: $@\n"
 
-set -euxo pipefail
-
-for i in {1..10}
-do
-  if curl -f $1 | jq --exit-status ".state == \"RUNNING\""; then
-    echo "Running"
-    exit 0
-  fi
-  echo "Not running"
-  sleep 10
+for var in "$@"; do
+  wait-for-url "$var"
 done
-
-echo "$0: Giving up after 10 attempts"
-exit 1
